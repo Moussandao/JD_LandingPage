575e787fd3f8dbd6c8a90356d1f7a493f46902d1
Moussandao <ndaomoussa07@gmail.com>
2025-12-30 15:53:01 +0100

    Fixing the code JavaScript for the contact formular

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const menu = document.querySelector(".navigation");
 const linkNav = document.querySelectorAll(".navigation a");
 const sections = document.querySelectorAll("section");
 const btnBurger = document.querySelector("#burger-menu");
+const SERVICE_ID = "service_j21e9by";
+const TEMPLATE_ID = "template_633h0jr";
+const PUBLIC_KEY = "DTxd8NIDLxUGeM72c";
 
 btnBurger.addEventListener("click", () => {
   menu?.classList.toggle("active");
@@ -59,3 +62,44 @@ sr.reveal(
   ".home-content, .home-img, .about-img, .about-content,.services-box, .menu-box, .contact-form, .btn, .section-heading",
   { interval: 200 }
 );
+/********Formulaire*****/
+emailjs.init(PUBLIC_KEY);
+
+const form = document.getElementById("ContactUs");
+const btn = form.querySelector("button");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Désactiver bouton + changer texte
+  btn.disabled = true;
+  btn.textContent = "En cours d'envoi...";
+
+  const params = {
+    user_name: document.getElementById("fullName").value,
+    user_email: document.getElementById("email").value,
+    user_message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send(SERVICE_ID, TEMPLATE_ID, params)
+    .then(() => {
+      btn.textContent = "Envoyé ✔️";
+      form.reset();
+
+      // Remettre le bouton à l’état normal après 3s
+      setTimeout(() => {
+        btn.disabled = false;
+        btn.textContent = "Envoyer";
+      }, 3000);
+    })
+    .catch(() => {
+      btn.textContent = "Réessayer ❌";
+      btn.disabled = false;
+    });
+});
+/******
+ * Public Key: "DTxd8NIDLxUGeM72c"
+ * Service ID: "service_znpdwc3"
+ * Template ID:  "template_633h0jr"
+ */
